954f15dd865eadddaf6bc63f8e826b45b46da1e8
AlexShevchenko <aashev4enko@gmail.com>
2024-10-16 12:41:26 +0300

    fixed menu display

--- src/main/java/telran/view/Menu.java
+++ src/main/java/telran/view/Menu.java
@@ -46,7 +46,7 @@ public class Menu implements Item {
 
     private void displayItems(InputOutput io) {
         IntStream.range(0, items.length).forEach(
-            i -> io.writeLine(String.format("%d. %s", i, items[i]))
+            i -> io.writeLine(String.format("%d. %s", i, items[i].displayName()))
         );
     }
 
